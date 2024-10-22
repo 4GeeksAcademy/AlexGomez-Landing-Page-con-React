a79b8db6811be208302e2aca03dd9bf4201de4bf
Alex Manuel Gomez Salazar <168044669+Lic-AlexGomez@users.noreply.github.com>
2024-10-22 04:58:58 +0000

    terminado

--- src/js/component/Footer/Footer.jsx
+++ src/js/component/Footer/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 import './Footer.css'
 function Footer() {
     return (
-        <footer className="footer bg-secondary  p-2 text-light text-center">
+        <footer className="footer bg-secondary  p-4 text-light text-center">
             <p>Copyright © Your Website 2018</p>
         </footer>
     );
--- src/js/component/Jumbotron/Jumbotron.jsx
+++ src/js/component/Jumbotron/Jumbotron.jsx
@@ -3,7 +3,7 @@ import './Jumbotron.css'
 export const Jumbotron = () => {
   return (
     <>
-      <div className="jumbotron C_jumbotron p-4">
+      <div className="jumbotron C_jumbotron p-5">
         <h1 className="display-4">A Warm, Welcome!</h1>
         <p className="lead">
 Lorem, ipsum dolor sit amet consectetur adipisicing elit. Id expedita, incidunt possimus animi provident in doloribus doloremque. Ullam impedit ratione explicabo corrupti nisi.        </p>
