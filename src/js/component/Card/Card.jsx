ba1c9bd66a03df81876334f685b5bc2e68068666
Alex Manuel Gomez Salazar <168044669+Lic-AlexGomez@users.noreply.github.com>
2024-10-24 02:16:27 +0000

    terminado

--- src/js/component/Card/Card.jsx
+++ src/js/component/Card/Card.jsx
@@ -1,52 +1,54 @@
 import React from "react"
-
+import './Card.css'
 
 export default function Card() {
     const cardData = [
       {
         id: 1,
         title: "Card title",
-        content: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente esse necessitatibus neque.",
+        content: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut ratione libero doloribus consequ fgsgd.",
+        imagen:"https://picsum.photos/seed/12345/500/325",
       },
       {
         id: 2,
         title: "Card title",
-        content: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Explicabo magni sapiente, tempore debitis beatae culpa natus architecto.",
+        content: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Explicabo magni sapiente, tempore.",
+        imagen:"https://picsum.photos/seed/12335/500/325",
       },
       {
         id: 3,
         title: "Card title",
         content: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente esse necessitatibus neque.",
+        imagen:"https://picsum.photos/seed/12745/500/325",
       },
       {
         id: 4,
         title: "Card title",
-        content: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Explicabo magni sapiente, tempore debitis beatae culpa natus architecto.",
+        content: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Explicabo magni sapiente, tempore.",
+        imagen:"https://picsum.photos/seed/16345/500/325",
       },
     ]
   
     return (
-      <div className="container mx-auto">
-        <div className="row row-cols-1 mt-4 row-cols-md-2 row-cols-lg-4 g-4">
-          {cardData.map((card) => (
-            <div key={card.id} className="col">
-              <div className="card h-100 border-0">
-                <img 
-                  src="https://picsum.photos/500/325" 
-                  className="card-img-top bg-light" 
-                  alt="Card image cap"
-                />
-                <div className="card-body px-0 d-flex flex-column justify-content-center ">
-                  <h5 className="card-title text-center">{card.title}</h5>
-                  <p className="card-text text-center">{card.content}</p>
+  
+        <div className="row row-cols-1 mt-4 row-cols-md-2  row-cols-lg-4 g-4">
+          {
+          cardData.map(({id,imagen,title,content}) => (
+
+
+            <div key={id} className="card text-center  mx-auto" >
+                <img src={imagen} className="card-img-top" alt="imagen carta"/>
+                <div className="card-body">
+                  <h5 className="card-title">{title}</h5>
+                  <p className="card-text">{content}</p>
+                  <hr/>
+                  <a href="#" className="btn btn-primary card_btn">Go somewhere</a>
                 </div>
-                <div className="card-footer bg-transparent border-0 px-0 mx-auto">
-                  <a href="#" className="btn btn-primary ">Find Out More!</a>
-                </div>
-              </div>
             </div>
+           
+        
           ))}
-        </div>
+     
       </div>
     )
   }
